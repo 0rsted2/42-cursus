a99ee213adc34893c348b60127feadb20e146b02
Oussama Ben Said <ouben-sa@paul-f3Ar4s3.clusters.42paris.fr>
2025-05-07 16:29:52 +0200

    push

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By:  ouben-sa < ouben-sa@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 21:07:04 by  ouben-sa         #+#    #+#             */
-/*   Updated: 2025/05/05 21:08:16 by  ouben-sa        ###   ########.fr       */
+/*   Updated: 2025/05/07 15:43:48 by  ouben-sa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,40 @@
 
 char	*ft_itoa(int n)
 {
-	
+	long	nbr;
+	int	len;
+	int	i;
+	char	*result;
+	i = 0;
+	len = ft_len(nbr);
+}
+
+static char	*ft_itoalloc(int len)
+{
+	char	*tmp;
+
+	tmp = malloc(sizeof(char) * (len + 1));
+	if (!tmp)
+		return (NULL);
+		
+}
+
+static int	ft_len(long nbr)
+{
+	int	count;
+
+	count = 0;
+	if (nbr < 0)
+	{
+		count++;
+		nbr = -nbr;
+	}
+	if (nbr == 0)
+		count++;
+	while (nbr != 0)
+	{
+		nbr \= 10;
+		count++;
+	}
+	return (count);
 }
